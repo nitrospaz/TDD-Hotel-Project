6d2c4ee259e817c063406697669292245d10cbe7
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2024-05-08 10:24:07 -0600

    Section 2 video 5 in progress

---
+++ RoomBookingApp.Core.Test/RoomBookingRequestProcessor.cs
@@ -0,0 +1,15 @@
+﻿
+namespace RoomBookingApp.Core
+{
+    internal class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        internal RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RoomBookingApp.Core.Test/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,40 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void ShouldReturnRoomBookingResponseWithValues()
+        {
+            // Arrange ---
+
+            // mock data
+            var bookingRequest = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2021, 1, 1)
+            };
+
+            // instantiate the processor
+            var processor = new RoomBookingRequestProcessor();
+
+
+            // Act --- 
+            RoomBookingResult result = processor.BookRoom(bookingRequest);
+
+
+            // Assert --- 
+            Assert.NotNull(result); // xunit assertion
+            result.ShouldNotBeNull(); // shouldly assertion
+        }
+    }
+}
